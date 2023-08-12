e6bdbf4cc9d8063122c10513129a88c279751cba
Camilaleilafernandez <camifernandez@frba.edu.ar>
2023-08-12 13:20:19 -0300

    primer repositorio

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	double pi=0.0;
+	double sum=0.0;
+	int sign =1;
+	int denominador=1;
+	while (abs(pi-3.141592)>0.000001)//abs devuelve el valor absoluto de un numero
+	{
+	sum =sum + sign*(1.0/denominador);
+	sign=sign*-1;
+	denominador= denominador + 2;
+		pi=4*sum;
+		
+		
+	}
+
+	cout<<"el valor aproximado de pi es :"<<fixed<<setprecision(6)<<pi<<endl;
+	return 0;
+} 
